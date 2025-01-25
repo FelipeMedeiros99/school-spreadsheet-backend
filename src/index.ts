89cc3b4e9bdffa320705b3c11e87d562cda65ea6
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-01-25 20:09:43 -0300

    feat: project started

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {json} from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express()
+app.use(cors())
+app.use(json())
+
+app.listen(process.env.PORT, ()=>console.log("app running"))
