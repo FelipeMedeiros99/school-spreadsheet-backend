c7cfefaefd8cf752b31f44342e7be3a7dd351301
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-01-25 21:44:58 -0300

    feat: handleError created

---
+++ src/middlewares/errorHandleMiddleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import internal from "stream";
+
+interface Error {
+  message: string;
+  status: number
+}
+
+export default function errorHandleMiddleware(err: Error, req: Request, res: Response, next: NextFunction){
+  try{ 
+    res.status(err?.status || 500).send(err.message || "internal server error")
+
+  }catch(e){
+    console.log(e)
+    res.send("Internal server error").status(500)
+  }
+}
