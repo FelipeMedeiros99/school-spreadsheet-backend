5f1f771e92abca7eb97ea9157388791d6544ae5c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-01-28 00:42:21 -0300

    feat: added userId to validation front end

--- src/controllers/authenticationControllers.ts
+++ src/controllers/authenticationControllers.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 
 export function signInController(req: Request, res: Response) {
-  res.status(200).send((req as any).token)
+  const responseData = {
+    token: (req as any).token,
+    userId: (req as any).userDataDatabase.id
+  }
+  res.status(200).send(responseData)
 }
 
 export function signUpController(req: Request, res: Response) {
